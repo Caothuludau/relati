4bd4a006118340d93167dafe6f9901c3b4772df7
CaoThuLuDau <npl.working@gmail.com>
2026-01-30 10:42:09 +0700

    chore(project): build project structure

--- src/main/java/com/winston/relati/RelatiApplication.java
+++ src/main/java/com/winston/relati/RelatiApplication.java
@@ -2,8 +2,13 @@ package com.winston.relati;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class
+        }
+)
 public class RelatiApplication {
 
     public static void main(String[] args) {
